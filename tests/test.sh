a2bc31f32c7194a9e2c7582eb9f51dd8e8027a78
mattjmcnaughton <mattjmcnaughton@gmail.com>
2018-02-08 09:55:50 -0500

    [WIP] Working on test suite

--- tests/test.sh
+++ tests/test.sh
@@ -3,12 +3,15 @@
 set -e
 
 test::check_syntax() {
+  ansible-playbook playbook.yml -i 'localhost' --syntax-check
 }
 
 test::run_ansible() {
+  ansible-playbook playbook.yml -i 'localhost'
 }
 
 test::assert_output() {
+  echo 'skipping'
 }
 
 test::check_syntax
